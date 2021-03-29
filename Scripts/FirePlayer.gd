8b07921e5968814affd25fcbe9d63911c6162e9a
urlordjames <32751441+urlordjames@users.noreply.github.com>
2021-03-29 15:24:27 -0400

    add particle to fire dash (#6)

--- Scripts/FirePlayer.gd
+++ Scripts/FirePlayer.gd
@@ -2,10 +2,16 @@ extends "res://Scripts/Player.gd"
 
 class_name FirePath
 
+const particle = preload("res://Scenes/FireDashParticle.tscn")
+
 func _init():
 	default_speed = 165
 
 func movement():
+	var inst: Node = particle.instance()
+	get_tree().get_root().add_child(inst)
+	inst.rotation = head.rotation
+	inst.global_transform = head.global_transform
 	move_cooldown = default_move_cooldown
 	velocity -= head_basis.z * 50
 	velocity.y = 0
